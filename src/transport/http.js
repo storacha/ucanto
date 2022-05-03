c3f962c8aeed83efc5dd227d95e49793b55071e0
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-05-03 15:33:24 -0400

    http transport supports injecting default http method, fetch impl

--- src/transport/http.js
+++ src/transport/http.js
@@ -1,38 +1,53 @@
-import * as Transport from "./api.js"
-import fetch from "@web-std/fetch"
+import * as Transport from "./api.js";
+import * as CAR from "./car.js";
 
 /**
  * @template T
- * @param {URL} url
+ * @param {object} options
+ * @param {typeof fetch} [options.fetch]
+ * @param {URL} options.url
+ * @param {string} [options.method]
  * @returns {Transport.Channel<T>}
  */
-export const open = url => new Channel({ url })
+export const open = ({ url, fetch  =  globalThis.fetch, method = 'POST', }) => {
+  if (typeof fetch === 'undefined') {
+    throw new TypeError(`ucanto HTTP transport got undefined \`fetch\`. Try passing in a \`fetch\` implementation explicitly.`)
+  }
+  return new Channel({ fetch, url, method });
+}
+
 class Channel {
   /**
    * @param {object} options
    * @param {URL} options.url
+   * @param {typeof fetch} options.fetch
+   * @param {string} [options.method]
    */
-  constructor({ url }) {
-    this.url = url
+  constructor({ url, fetch, method }) {
+    this.fetch = fetch;
+    this.method = method;
+    this.url = url;
   }
   /**
    * @param {Transport.HTTPRequest} request
    * @returns {Promise<Transport.HTTPResponse>}
    */
   async request({ headers, body }) {
-    const response = await fetch(this.url.href, {
+    const decodedBody = await CAR.decode({ headers, body });
+    const response = await this.fetch(this.url.href, {
       headers,
       body,
-    })
+      method: this.method
+    });
 
     const buffer = response.ok
       ? await response.arrayBuffer()
-      : HTTPError.throw("HTTP Request failed", response)
+      : HTTPError.throw("HTTP Request failed", response);
 
     return {
       headers: Object.fromEntries(response.headers.entries()),
       body: new Uint8Array(buffer),
-    }
+    };
   }
 }
 
@@ -50,16 +65,16 @@ class HTTPError extends Error {
    * @returns {never}
    */
   static throw(message, options) {
-    throw new this(message, options)
+    throw new this(message, options);
   }
   /**
    * @param {string} message
    * @param {Options} options
    */
   constructor(message, { url, status = 500, statusText = "Server error" }) {
-    super(message)
-    this.url = url
-    this.status = status
-    this.statusText = statusText
+    super(message);
+    this.url = url;
+    this.status = status;
+    this.statusText = statusText;
   }
 }
