4fa68762ca69f304515d56f9b658ddba30fb51a7
Irakli Gozalishvili <contact@gozala.io>
2022-06-23 08:04:31 -0700

    fix(ucanto): capability provider inference (#52)

--- packages/server/src/api.ts
+++ packages/server/src/api.ts
@@ -1,5 +1,5 @@
 import * as API from '@ucanto/interface'
-import { InferCaveats, CanIssue } from '@ucanto/interface'
+import { InferCaveats, CanIssue, ParsedCapability } from '@ucanto/interface'
 
 export * from '@ucanto/interface'
 
@@ -25,6 +25,13 @@ export interface ProviderContext<
 > {
   capability: API.ParsedCapability<A, R, API.InferCaveats<C>>
   invocation: API.Invocation<API.Capability<A, R['href']> & API.InferCaveats<C>>
+
+  context: API.InvocationContext
+}
+
+export interface ProviderInput<T extends ParsedCapability> {
+  capability: T
+  invocation: API.Invocation<API.Capability<T['can'], T['with']> & T['caveats']>
 
   context: API.InvocationContext
 }
