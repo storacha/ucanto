67d3ab4c53ed2ad0c6ac3e0ce363e97ceb472db0
Irakli Gozalishvili <contact@gozala.io>
2022-06-23 03:32:09 -0700

    fix(ucanto): capability provider inference

--- packages/server/src/handler.js
+++ packages/server/src/handler.js
@@ -2,15 +2,11 @@ import * as API from './api.js'
 import { access } from '@ucanto/validator'
 
 /**
- * @template {API.Ability} A
- * @template {API.Caveats} C
- * @template {API.URI} R
+ * @template {API.ParsedCapability} T
  * @template {unknown} U
- * @template {API.Match} Z
- * @template {API.ParsedCapability<A, R, API.InferCaveats<C>>} T
- * @param {API.CapabilityParser<API.Match<T, Z>>} capability
- * @param {(input:API.ProviderContext<A, R, C>) => API.Await<U>} handler
- * @returns {API.ServiceMethod<API.Capability<A, R['href']> & API.InferCaveats<C>, Exclude<U, {error:true}>, Exclude<U, Exclude<U, {error:true}>>>}
+ * @param {API.CapabilityParser<API.Match<T>>} capability
+ * @param {(input:API.ProviderInput<T>) => API.Await<U>} handler
+ * @returns {API.ServiceMethod<API.Capability<T['can'], T['with']> & T['caveats'], Exclude<U, {error:true}>, Exclude<U, Exclude<U, {error:true}>>>}
  */
 
 export const provide =
@@ -18,7 +14,6 @@ export const provide =
   /**
    * @param {API.Invocation<API.Capability<T['can'], T['with']> & T['caveats']>} invocation
    * @param {API.InvocationContext} options
-   * @return {Promise<API.Result<Exclude<U, {error:true}>, Exclude<U, Exclude<U, {error:true}>>|API.InvocationError>>}
    */
   async (invocation, options) => {
     const authorization = await access(invocation, {
