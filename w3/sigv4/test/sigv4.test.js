f78add2a732aa7e64073a0aacbf476240a64326f
Irakli Gozalishvili <contact@gozala.io>
2022-06-28 22:48:47 -0700

    fix: use chai assert

--- w3/sigv4/test/sigv4.test.js
+++ w3/sigv4/test/sigv4.test.js
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import { assert } from 'chai'
 import { sha256 } from '@noble/hashes/sha256'
 import { SigV4 } from '../src/index.js'
 import fetch from '@web-std/fetch'
