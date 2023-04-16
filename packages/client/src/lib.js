7b4dcb3bf182389ac17ac088b11504769a54a773
Irakli Gozalishvili <contact@gozala.io>
2023-04-16 01:16:34 -0700

    explicitly export conflicting names

--- packages/client/src/lib.js
+++ packages/client/src/lib.js
@@ -1,3 +1,4 @@
 export * from './connection.js'
 export * from '@ucanto/interface'
 export * from '@ucanto/core'
+export { API, Receipt, Signature, DID, Delegation, Failure, Invocation } from '@ucanto/core'
