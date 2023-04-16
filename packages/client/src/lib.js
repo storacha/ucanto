af7aa82c9b3efa38d9ddacd85ef816b05dca23b0
Irakli Gozalishvili <contact@gozala.io>
2023-04-16 01:09:58 -0700

    fix: export core from the client

--- packages/client/src/lib.js
+++ packages/client/src/lib.js
@@ -1,7 +1,4 @@
 export * from './connection.js'
 
 export * from '@ucanto/interface'
-import { Delegation, invoke } from '@ucanto/core'
-
-export const delegate = Delegation.delegate
-export { invoke }
+import * from '@ucanto/core'
