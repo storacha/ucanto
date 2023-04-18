dc5f834d88b2aa246e161f67ae91d2e6fbc4c99a
Irakli Gozalishvili <contact@gozala.io>
2023-04-18 15:33:31 +0200

    fix: amend delegation type

--- packages/interface/src/lib.ts
+++ packages/interface/src/lib.ts
@@ -241,6 +241,8 @@ export interface Delegation<C extends Capabilities = Capabilities>
 
   toJSON(): DelegationJSON<this>
   delegate(): Await<Delegation<C>>
+
+  archive(): Await<Result<Uint8Array, Error>>
 }
 
 /**
