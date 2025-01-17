deb30a3de09fb0dc1d703635c21a254456431092
Hannah Howard <hannah@hannahhoward.net>
2025-01-17 13:25:20 -0800

    feat(core): add schema type for reading did bytes (#360)
    * feat(core): add schema type for reading did bytes
    
    * Update packages/core/src/schema.js
    
    Co-authored-by: ash <alan@storacha.network>
    
    ---------
    
    Co-authored-by: ash <alan@storacha.network>

--- packages/core/src/schema/did.js
+++ packages/core/src/schema/did.js
@@ -1,6 +1,6 @@
 import * as API from '@ucanto/interface'
 import * as Schema from './schema.js'
-
+import * as DID from '@ipld/dag-ucan/did'
 /**
  * @template {string} Method
  * @extends {Schema.API<API.DID<Method> & API.URI<"did:">, string, void|Method>}
@@ -43,3 +43,55 @@ export const match = (options = {}) =>
  * @param {unknown} input
  */
 export const from = input => match({}).from(input)
+
+/**
+ * @template {string} Method
+ * @extends {Schema.API<API.DID<Method> & API.URI<"did:">, unknown, void|Method>}
+ */
+class DIDBytesSchema extends Schema.API {
+  /**
+   * @param {unknown} source
+   * @param {void|Method} method
+   */
+  readWith(source, method) {
+    if (!(source instanceof Uint8Array)) {
+      return Schema.typeError({ expect: 'Uint8Array', actual: source })
+    }
+    let did
+    try {
+      did = DID.decode(source).did()
+    } catch (err) {
+      return Schema.error(`Unable to parse bytes as did: ${err}`)
+    }
+    const prefix = method ? `did:${method}:` : `did:`
+    if (!did.startsWith(prefix)) {
+      return Schema.error(`Expected a ${prefix} but got "${did}" instead`)
+    } else {
+      return { ok: /** @type {API.DID<Method>} */ (did) }
+    } 
+  }
+}
+
+const schemaBytes = new DIDBytesSchema()
+
+export const didBytes = () => schemaBytes
+/**
+ *
+ * @param {unknown} input
+ */
+export const readBytes = input => schemaBytes.read(input)
+
+/**
+ * @template {string} Method
+ * @param {{method?: Method}} options
+ */
+export const matchBytes = (options = {}) =>
+  /** @type {Schema.Schema<API.DID<Method> & API.URI<"did:">>} */ (
+    new DIDBytesSchema(options.method)
+  )
+
+/**
+ * Create a DID string from any input (or throw)
+ * @param {unknown} input
+ */
+export const fromBytes = input => matchBytes({}).from(input)
